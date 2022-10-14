8c37e5a680203d19bf7908c912171ac5c071366f
IshaanSharma <84267522+Dragoi5@users.noreply.github.com>
2022-10-15 06:29:17 +1100

    Added helper class

--- app/src/main/java/com/example/wireframes_trial1/Profile.java
+++ app/src/main/java/com/example/wireframes_trial1/Profile.java
@@ -3,12 +3,31 @@ package com.example.wireframes_trial1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ImageView;
 
 public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_home);
+
+        ImageView home = findViewById(R.id.homeIcon);
+        ImageView resources = findViewById(R.id.resourcesIcon);
+        ImageView blog = findViewById(R.id.blogIcon);
+        ImageView profile = findViewById(R.id.profileIcon);
+
+        Helper startup = new Helper("home",this,home,resources,blog,profile);
+        startup.navigationBar("Home");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        System.out.println("executed");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_profile, menu);
+        return true;
     }
 }
